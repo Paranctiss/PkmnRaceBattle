dae6037032cd48a2866c5e21cae193a0e82d5b5c
Paranctiss <fli1@live.fr>
2026-01-29 23:38:28 +0100

    draft nouveau fond + position relative de merde

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,19 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {routeAnimations} from './shared/animations/route.animations';
 import {ImageConfigService} from './core/services/ImageConfigService/image-config.service';
+import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   animations: [routeAnimations]
 })
 export class AppComponent {
   title = 'PkmnRaceBattle';
+  environment: string = "plains";
+
   prepareRoute(outlet: RouterOutlet) {
     return outlet?.activatedRouteData?.['animation'];
   }
@@ -21,4 +24,10 @@ export class AppComponent {
   ngOnInit() {
     this.imageConfigService.disableDragForAllImages();
   }
+
+  changeEnvironment(environment: string) {
+    this.environment = environment;
+  }
+
+
 }
